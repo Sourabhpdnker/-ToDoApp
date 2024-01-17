eb311587ab274eb28f1825446f6fe41cd53eb9f7
Sourabh <137615880+Sourabhpdnker@users.noreply.github.com>
2024-01-18 01:22:20 +0530

    date function added

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-let todoList = [];
+let todoList = [
+    // {
+    //     item: 'study OS ',
+    //     duedate: '04/01/2024',
+    // },
+    // {
+    //     item: 'study java ',
+    //     duedate: '04/01/2024',
+    // },
+    // {
+    //     item: '',
+    //     duedate: '',
+    // }
+    //by using above concept we have created arrray of object
+
+];
 displayTodoItem();
 
 
 function addToDo() {
     let inputElement = document.querySelector('#task-input');
+    let dateElement = document.querySelector('#date-input');
     let todoItem = inputElement.value;
-    todoList.push(todoItem);
+    let dateItem = dateElement.value;
+    todoList.push({item:todoItem , duedate : dateItem});
     inputElement.value = '';
+    dateElement.value = '';
 
     displayTodoItem();
 }
@@ -16,11 +34,16 @@ function displayTodoItem(){
     let newHtml = '';
 
     for(let i=0;i<todoList.length;i++){
+        let item = todoList[i].item;
+        let duedate= todoList[i].duedate;
         newHtml += `
             <div class="task-container2">
-            <h3>${i+1} : ${todoList[i]}</h3>
+            <h4>${i+1} : ${item}</h4>
+            <div class="task-con-child">
+            <p>${duedate}</p>
             <button class="delete-button" onclick="todoList.splice(${i} , 1);
             displayTodoItem();">Delete</button>
+            </div>
             </div>
              `;
     }
